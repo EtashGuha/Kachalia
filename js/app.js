877b8348bf23c1cd06557ce84dc23a467d3db255
Etash Guha <etashthebomb@gmail.com>
2020-12-29 03:28:20 -0500

    working

--- js/app.js
+++ js/app.js
@@ -59,7 +59,6 @@
                               currScore += 1
                             }
                           })
-                          console.log(currScore)
                           map.set(key, map.get(key) + currScore)
                           var indexToInsert = sortedIndex(topScores, currScore);
                           topScores.splice(indexToInsert, 0, key)
